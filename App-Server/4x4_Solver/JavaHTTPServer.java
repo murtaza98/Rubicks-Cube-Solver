63125713fb98e224094d2a1b6c5e9410637a874d
murtaza98 <m.patrawala@somaiya.edu>
2020-01-28 23:09:35 +0530

    integrated 4x4 cube solver and java server

--- App-Server/4x4_Solver/JavaHTTPServer.java
+++ App-Server/4x4_Solver/JavaHTTPServer.java
@@ -1,21 +1,15 @@
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
-import java.util.StringTokenizer;
-import java.io.StringReader;
 import java.util.Hashtable;
+import cs.threephase.Search;
 
-// The tutorial can be found just here on the SSaurel's Blog : 
-// https://www.ssaurel.com/blog/create-a-simple-http-web-server-in-java
+
 // Each Client Connection will be managed in a dedicated Thread
 public class JavaHTTPServer implements Runnable{ 
 	
@@ -26,7 +20,9 @@ public class JavaHTTPServer implements Runnable{
 	static final boolean verbose = true;
 	
 	// Client Connection via Socket Class
-	private Socket socket;
+    private Socket socket;
+    
+    private static Search search = new Search();
 	
 	public JavaHTTPServer(Socket c) {
 		socket = c;
@@ -35,7 +31,7 @@ public class JavaHTTPServer implements Runnable{
 	public static void main(String[] args) {
 		try {
 			ServerSocket serverConnect = new ServerSocket(PORT);
-			System.out.println("Server started.\nListening for connections on port : " + PORT + " ...\n");
+			System.out.println("Server started.\nListening for connections on port : " + PORT + " ...");
 			
 			// we listen until user halts server execution
 			while (true) {
@@ -59,8 +55,6 @@ public class JavaHTTPServer implements Runnable{
 	public void run() {
 		// we manage our particular client connection
 		BufferedReader in = null; PrintWriter out = null; BufferedOutputStream dataOut = null;
-		String fileRequested = null;
-		
 		try {
 			// we read characters from the client via input stream on the socket
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -75,77 +69,33 @@ public class JavaHTTPServer implements Runnable{
 
 
             // System.out.println(parser.getRequestLine());
-            System.out.println(parser.getMessageBody());
-		    
-		    
-		    
-		    
+            System.out.println(parser.getRequestLine());
+
+            String[] tmp = parser.getRequestLine().split("\\s+");
+            String GETParam = tmp[1].substring(2);
+            String scrambleMoves = GETParam.split("=")[1];
+            scrambleMoves = scrambleMoves.replaceAll("_", " ");
+
+            System.out.println(scrambleMoves);
+
+            String solutionMoves = search.solve(scrambleMoves);
+            System.out.println(solutionMoves);
+            
+            
+            // CREATE RESPONSE
+
+            // send HTTP Headers
+            String contentType = "text/plain";
+            String messageLength = String.valueOf(solutionMoves.length()+1);
+            out.println("HTTP/1.1 200 OK");
+            out.println("Server: Java HTTP Server from SSaurel : 1.0");
+            out.println("Date: " + new Date());
+            out.println("Content-type: " + contentType);
+            out.println("Content-length: " + messageLength);
+            out.println(); // blank line between headers and content, very important !
+            out.println(solutionMoves);
+            out.flush();
 			
-			
-//			// get first line of the request from the client
-//			String input = in.readLine();
-//			// we parse the request with a string tokenizer
-//			StringTokenizer parse = new StringTokenizer(input);
-//			String method = parse.nextToken().toUpperCase(); // we get the HTTP method of the client
-//			// we get file requested
-//			fileRequested = parse.nextToken().toLowerCase();
-//			
-//			// we support only GET and HEAD methods, we check
-//			if (!method.equals("GET")  &&  !method.equals("HEAD")) {
-//				if (verbose) {
-//					System.out.println("501 Not Implemented : " + method + " method.");
-//				}
-//				
-//				// we return the not supported file to the client
-//				File file = new File(WEB_ROOT, METHOD_NOT_SUPPORTED);
-//				int fileLength = (int) file.length();
-//				String contentMimeType = "text/html";
-//				//read content to return to client
-//				byte[] fileData = readFileData(file, fileLength);
-//					
-//				// we send HTTP Headers with data to client
-//				out.println("HTTP/1.1 501 Not Implemented");
-//				out.println("Server: Java HTTP Server from SSaurel : 1.0");
-//				out.println("Date: " + new Date());
-//				out.println("Content-type: " + contentMimeType);
-//				out.println("Content-length: " + fileLength);
-//				out.println(); // blank line between headers and content, very important !
-//				out.flush(); // flush character output stream buffer
-//				// file
-//				dataOut.write(fileData, 0, fileLength);
-//				dataOut.flush();
-//				
-//			} else {
-//				// GET or HEAD method
-//				if (fileRequested.endsWith("/")) {
-//					fileRequested += DEFAULT_FILE;
-//				}
-//				
-//				File file = new File(WEB_ROOT, fileRequested);
-//				int fileLength = (int) file.length();
-//				String content = getContentType(fileRequested);
-//				
-//				if (method.equals("GET")) { // GET method so we return content
-//					byte[] fileData = readFileData(file, fileLength);
-//					
-//					// send HTTP Headers
-//					out.println("HTTP/1.1 200 OK");
-//					out.println("Server: Java HTTP Server from SSaurel : 1.0");
-//					out.println("Date: " + new Date());
-//					out.println("Content-type: " + content);
-//					out.println("Content-length: " + fileLength);
-//					out.println(); // blank line between headers and content, very important !
-//					out.flush(); // flush character output stream buffer
-//					
-//					dataOut.write(fileData, 0, fileLength);
-//					dataOut.flush();
-//				}
-//				
-//				if (verbose) {
-//					System.out.println("File " + fileRequested + " of type " + content + " returned");
-//				}
-//				
-//			}
 			
 		} catch ( Exception e ){
 			System.out.println("Server error : " + e);
@@ -163,18 +113,7 @@ public class JavaHTTPServer implements Runnable{
 				System.out.println("Connection closed.\n");
 			}
 		}
-		
-		
-	}
-	
-	// return supported MIME Types
-	private String getContentType(String fileRequested) {
-		if (fileRequested.endsWith(".htm")  ||  fileRequested.endsWith(".html"))
-			return "text/html";
-		else
-			return "text/plain";
-	}
-	
+	}	
 }
 
 
@@ -211,18 +150,19 @@ class HttpRequestParser {
      */
     public void parseRequest(BufferedReader reader) throws IOException, HttpFormatException {
         setRequestLine(reader.readLine()); // Request-Line ; Section 5.1
-
-        String header = reader.readLine();
-        while (header.length() > 0) {
-            appendHeaderParameter(header);
-            header = reader.readLine();
-        }
-
+        
+        String header = "";
         while(reader.ready()){
-            String bodyLine = reader.readLine().trim();
-            appendMessageBody(bodyLine);
-        }      
-
+            char c  = (char)reader.read();
+            if(c=='\n' || c== -1){
+                // System.out.println("header --> "+ header);
+                appendHeaderParameter(header);
+                header = "";
+            }else{
+                header += c;
+            }
+            
+        } 
     }
 
     /**
@@ -237,6 +177,7 @@ class HttpRequestParser {
     public String getRequestLine() {
         return _requestLine;
     }
+    
 
     private void setRequestLine(String requestLine) throws HttpFormatException {
         if (requestLine == null || requestLine.length() == 0) {
@@ -248,7 +189,8 @@ class HttpRequestParser {
     private void appendHeaderParameter(String header) throws HttpFormatException {
         int idx = header.indexOf(":");
         if (idx == -1) {
-            throw new HttpFormatException("Invalid Header Parameter: " + header);
+            return;
+            // throw new HttpFormatException("Invalid Header Parameter: " + header);
         }
         // System.out.println(String.format("%s %s", header.substring(0, idx), header.substring(idx + 1, header.length())));
         _requestHeaders.put(header.substring(0, idx), header.substring(idx + 1, header.length()));
@@ -282,11 +224,7 @@ class HttpRequestParser {
 
 class HttpFormatException extends Exception
 {
-
-	/**
-	 * 
-	 */
-	private static final long	serialVersionUID	= 1L;
+	private static final long serialVersionUID	= 1L;
 
 	public HttpFormatException(String arg0)
     {
